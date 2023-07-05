189278358843e6813665568a19f40307a6e1f9dd
VARZero <cydiazero@gmail.com>
2023-07-05 21:13:48 +0900

    emnist

--- model.py
+++ model.py
@@ -1,1 +1,8 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
+
+import matplotlib as plt
+import numpy as np
+
+(ds_train, ds_test), ds_info = tfds.load('emnist/letters', split=['train', 'test'], shuffle_files=True, as_supervised=True, with_info=True)
+print(ds_train)
