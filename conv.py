7477bdf8a459b2bd5bc1a7f8bbbf35c8d8a6399c
VARZero <cydiazero@gmail.com>
2023-08-20 00:31:13 +0900

    새로 학습시킨 모델

--- conv.py
+++ conv.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 import tensorflowjs as tfjs
 
-cnnEmnist = tf.keras.models.load_model('./model/spemnist.h5')
+cnnEmnist = tf.keras.models.load_model('./model/spemnist.keras')
 tfjs.converters.save_keras_model(cnnEmnist, './model/cnnemnistjs')
